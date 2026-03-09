154b2d98ac92303cacaa6154bcd0e5982a8aaa47
Matthew Morrison <matthew@mattymo.dev>
2026-03-09 12:06:17 -0400

    Fix bugs with respawning (#36)

--- Assets/Scripts/TankRespawner.cs
+++ Assets/Scripts/TankRespawner.cs
@@ -5,55 +5,64 @@ public class TankRespawner : MonoBehaviour
 {
     public float respawnDelay = 3f;
     public float invulnerabilityTime = 2f;
+    public GameObject tank;
+    private Shooter[] shooters;
+    private SpriteRenderer[] renderers;
+    private Collider2D[] colliders;
+    private ParticleSystem[] particleSystems;
 
     private Vector3 spawnPosition;
     private bool isInvulnerable = false;
 
-    // References
-    private SpriteRenderer spriteRenderer;
-    private Collider2D bodyCollider;
-
     void Awake()
     {
-        Collider2D[] colliders = GetComponents<Collider2D>();
-
-        foreach (var col in colliders)
-        {
-            if (!col.isTrigger) this.bodyCollider = col;
-        }
-
-        this.spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        shooters = tank.GetComponents<Shooter>();
+        renderers = tank.GetComponentsInChildren<SpriteRenderer>();
+        colliders = tank.GetComponentsInChildren<Collider2D>();
+        particleSystems = tank.GetComponentsInChildren<ParticleSystem>();
     }
 
     void Start()
     {
-        this.spawnPosition = this.transform.position;
+        this.spawnPosition = this.tank.transform.position;
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    public void Die(Collider2D other)
     {
-        if (!collision.CompareTag("Pellet") || this.isInvulnerable) return;
-        else this.Die();
-    }
+        if (!other.CompareTag("Pellet") || this.isInvulnerable) return;
 
-    void Die()
-    {
         StartCoroutine(RespawnRoutine());
+
+        foreach (var shooter in shooters)
+        {
+            shooter.SetCount(0);
+        }
+    }
+
+    void SetTankActive(bool enabled)
+    {
+        foreach (var renderer in renderers)
+        {
+            renderer.enabled = enabled;
+        }
+        foreach (var collider in colliders)
+        {
+            collider.enabled = enabled;
+        }
+        foreach (var system in particleSystems)
+        {
+            system.gameObject.SetActive(enabled);
+        }
     }
 
     IEnumerator RespawnRoutine()
     {
-        this.spriteRenderer.enabled = false;
-        this.bodyCollider.enabled = false;
-        yield return new WaitForSeconds(this.respawnDelay);
-
-        // Start invulnerability
         this.isInvulnerable = true;
+        SetTankActive(false);
 
-        // Respawn
-        this.transform.position = this.spawnPosition;
-        this.spriteRenderer.enabled = true;
-        this.bodyCollider.enabled = true;
+        yield return new WaitForSeconds(this.respawnDelay);
+        tank.transform.position = this.spawnPosition;
+        SetTankActive(true);
 
         yield return new WaitForSeconds(this.invulnerabilityTime);
         this.isInvulnerable = false;
